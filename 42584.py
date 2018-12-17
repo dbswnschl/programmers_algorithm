68c1811175a0c2a4b35a5cbbda677f088e482225
unknown <dbswnschl@suwon.ac.kr>
2018-12-17 15:10:33 +0900

    add 42584

---
+++ 42584.py
@@ -0,0 +1,15 @@
+def solution(prices):
+    answer = [0 for i in range(len(prices))]
+
+    arr_stack = []
+    for i in range(len(prices)):
+        for j in range(i+1,len(prices),1):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+    return answer
+
+
+print(solution([498,501,470,489]))
